59ac22136f4aac6a33c01a207858d6aa2e4c2b91
jspark300 <2525js@naver.com>
2017-04-26 00:47:18 +0900

    singleInstance

--- app/src/main/java/com/giant_monkey/broadcast/MainActivity.java
+++ app/src/main/java/com/giant_monkey/broadcast/MainActivity.java
@@ -10,4 +10,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
 }
